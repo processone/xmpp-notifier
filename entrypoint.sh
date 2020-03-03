9e09f8f5e5b641508a06bd101604440b03ea586b
rcorniere <rcorniere@process-one.net>
2020-03-03 12:49:57 +0100

    Remove checkout from workflow

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+go get -u https://github.com/FluuxIO/go-xmpp
 go run /notifier/main.go "${INPUT_SERVER_DOMAIN}" "${INPUT_CORRESPONDANT}" "$INPUT_LOGIN" "${INPUT_PASS}" "${INPUT_SERVER_PORT}" "${INPUT_MESSAGE}" "${INPUT_CORRESPONDENT_IS_ROOM}"
 
